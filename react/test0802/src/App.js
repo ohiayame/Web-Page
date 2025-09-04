f45231725ace21248cafaa97788e7014807fae6a
ohiayame <0819aym001@gmail.com>
2025-09-04 23:38:53 +0900

    react youtubeApi channel search

--- react/test0802/src/App.js
+++ react/test0802/src/App.js
@@ -1,76 +1,43 @@
-import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from "uuid";
-import { useState } from 'react';
-import { addPost, deletePost } from './features/Posts';
+import React, { useState, useEffect } from "react";
+import api from "./api/api";
 
 function App() {
-  // 제목의 useState()
-  const [title, setTitle] = useState("");
-  // 내용의 useState()
-  const [content, setContent] = useState("");
-  // useSelector()로 store(posts의 initialState.value)에 접근
-  const postList = useSelector((state) => state.posts.value);
-  // dispatch선언
-  const dispatch = useDispatch();
+  const [videos, setVideos] = useState([]);
 
-  // 개시물을 추가
-  const handleAddPost = () => {
-    // 내용이 없으면 무시
-    if(title === "" || content === "") return;
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await api.get("/search", {
+          params: {
+            q: "영진전문대학교",
+          },
+        });
+        setVideos(response.data.items);
+      } catch (error) {
+        console.error("Error fetching videos:", error);
+      }
+    };
 
-    // dispatch로 addPost(id, title, content) -> title, content 초기화
-    dispatch(
-      addPost({
-        id: uuidv4(),
-        title: title,
-        content: content
-      })
-    );
-    setTitle("");
-    setContent("");
-  }
+    fetchVideos();
+  }, []);
 
-  return(
+  console.log(videos);
+  return (
     <div>
-      <div>
-        <h1>개시판</h1>
-      </div>
-
-      <div className='addPost'>
-        {/* 제목 */}
-        <input
-          type='text'
-          placeholder='title'
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        {/* 내용 */}
-        <input 
-          type='text'
-          placeholder='content'
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-        />
-        {/* 저장 버튼 */}
-        <button onClick={() => handleAddPost()}>upLoad</button>
-        <hr />
-      </div>
-
-      {/* 출력 */}
-      <div className='displayPosts'>
-        {postList.map((post) =>(
-          <div className='post' key={post.id}>
-            <h1 className='postName'>{post.title}</h1>
-            <h1 className='postContent'>{post.content}</h1>
-            <button onClick={() => dispatch(deletePost({id: post.id}))}>
-              delete
-            </button>
-          </div>
+      <h2>YouTube Clone</h2>
+      <ul>
+        {videos.map((video) => (
+          <li key={video.id.videoId}>
+            <img
+              src={video.snippet.thumbnails.medium.url}
+              alt={video.snippet.title}
+            />
+            <p>{video.snippet.title}</p>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
--- react/test0802/src/index.js
+++ react/test0802/src/index.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from 'react-redux';
-import postsReducer from "./features/Posts"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-//  store를 생성
-const store = configureStore({
-  reducer: {
-    posts: postsReducer,
-  }
-})
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
-
